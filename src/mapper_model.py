fef110220e6a875664dc59065731412aff832dec
Peer Juergens <peerjuergens2000@gmail.com>
2024-12-10 21:11:33 +0000

    adjustments normal mapper

--- src/mapper_model.py
+++ src/mapper_model.py
@@ -24,9 +24,9 @@ def mapper_model(input_shape=(192,), latent_dim=8):
     mapper_inputs = Input(shape=input_shape)
     
     x = Dense(128, activation="relu")(mapper_inputs)
-    #x = Dropout(0.2)(x)
+    x = Dropout(0.2)(x)
     x = Dense(64, activation="relu")(x)
-    #x = Dropout(0.2)(x)
+    x = Dropout(0.2)(x)
     x = Dense(32, activation="relu")(x)
     x = Dense(16, activation="relu")(x)
     x = Dense(latent_dim, activation="relu")(x)
